b61ef6018cd0fab1d42cf7053668ed78a3f38f63
Davy Hausser <davy.hausser@gmail.com>
2021-05-31 22:50:05 +0200

    Layout styles

---
+++ components/layout.tsx
@@ -0,0 +1,28 @@
+import Link from 'next/link'
+
+export function Layout({ children }) {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/">
+              <a>Home</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/about">
+              <a>About Us</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/blog/hello-world">
+              <a>Blog Post</a>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      {children}
+    </div>
+  )
+}
